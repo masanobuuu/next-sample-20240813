e19da2177f2c7d634c9e9444c02db604032f1d1a
MASANOBU\emasa <e.masanobuuu@gmail.com>
2024-08-12 21:27:32 +0900

    _app.tsxにthemeを設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { theme } from 'themes'
 
 // グローバルのスタイル
 const GlobalStyle = createGlobalStyle`
@@ -31,28 +32,30 @@ ol, ul {
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <Head>
-        <meta key="charset" name="charset" content="utf-8" />
-        <meta
-          key="viewport"
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
-        />
-        {/* <title key="title">{SITE_TITLE}</title>
-        <meta name="title" content={SITE_TITLE} key="meta:title" />
-        <meta name="description" content={SITE_DESCRIPTION} key="meta:description" />
-        <meta property="og:title" content={SITE_TITLE} key="meta:og:title" />
-        <meta property="og:description" content={SITE_DESCRIPTION} key="meta:og:description" />
-        <meta property="og:image" content={`${publicRuntimeConfig.domainUrl}/static/images/icon/icon-512.png`} key="meta:og:image" />
-        <meta property="og:site_name" content={SITE_NAME} /> */}
-        <meta property="og:locale" content="ja_JP" />
-        <meta property="og:type" content="website" />
-        {/* <meta property="fb:app_id" content="556485011968079" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@truck2hand" /> */}
-      </Head>
-      <GlobalStyle />
-      <Component {...pageProps} />
+     <ThemeProvider theme={theme}>
+        <Head>
+          <meta key="charset" name="charset" content="utf-8" />
+          <meta
+            key="viewport"
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+          />
+          {/* <title key="title">{SITE_TITLE}</title>
+          <meta name="title" content={SITE_TITLE} key="meta:title" />
+          <meta name="description" content={SITE_DESCRIPTION} key="meta:description" />
+          <meta property="og:title" content={SITE_TITLE} key="meta:og:title" />
+          <meta property="og:description" content={SITE_DESCRIPTION} key="meta:og:description" />
+          <meta property="og:image" content={`${publicRuntimeConfig.domainUrl}/static/images/icon/icon-512.png`} key="meta:og:image" />
+          <meta property="og:site_name" content={SITE_NAME} /> */}
+          <meta property="og:locale" content="ja_JP" />
+          <meta property="og:type" content="website" />
+          {/* <meta property="fb:app_id" content="556485011968079" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@truck2hand" /> */}
+        </Head>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
