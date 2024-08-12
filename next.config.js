f4d6a09841ae82dfc1c8623d810832ba72f72c3d
MASANOBU\emasa <e.masanobuuu@gmail.com>
2024-08-12 19:44:39 +0900

    styled-componentsの設定

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    
   reactStrictMode: true,
-  swcMinify: true,
+  
+  compiler: {
+	//styledComponentsの有効化
+	styledComponents: true,
+  },
 }
 
 module.exports = nextConfig
