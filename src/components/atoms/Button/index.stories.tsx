ee5435cbfc2b5adb3eafdee9e9cbc960948a5bc1
MASANOBU\emasa <e.masanobuuu@gmail.com>
2024-08-13 09:07:25 +0900

    Boxコンポーネントを新規作成

---
+++ src/components/atoms/Button/index.stories.tsx
@@ -0,0 +1,77 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Button from './index'
+
+export default {
+  title: 'Atoms/Button',
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary'],
+      control: { type: 'radio' },
+      defaultValue: 'primary',
+      // docsに表示する内容を設定
+      description: 'ボタンバリアント',
+      table: {
+        type: { summary: 'primary | secondary' },
+        defaultValue: { summary: 'primary' },
+      },
+    },
+    children: {
+      control: { type: 'text' },
+      defaultValue: 'Button',
+      description: 'ボタンテキスト',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    disabled: {
+      control: { type: 'boolean' },
+      defaultValue: false,
+      description: 'Disabledフラグ',
+      table: {
+        type: { summary: 'boolean' },
+      },
+    },
+    width: {
+      control: { type: 'number' },
+      description: 'ボタンの横幅',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    height: {
+      control: { type: 'number' },
+      description: 'ボタンの縦幅',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    onClick: {
+      description: 'onClickイベントハンドラ',
+      table: {
+        type: { summary: 'function' },
+      },
+    },
+  },
+} as Meta<typeof Button>
+
+
+// Primaryボタン
+export const Primary: StoryObj = {
+  args: { 
+    variant: 'primary', 
+    children: 'Primary Button' }
+}
+
+// Secondaryボタン
+export const Secondary: StoryObj = {
+  args: { 
+    variant: 'econdary', 
+    children: 'Secondary Button' }
+}
+
+// Disabledボタン
+export const Disabled: StoryObj = {
+  args: { 
+    disabled: true, 
+    children: 'Disabled Button' }
+}
