69e732134cc46087b9d46540f399d2610cb6ff14
MASANOBU\emasa <e.masanobuuu@gmail.com>
2024-08-13 20:03:17 +0900

    Dropdownコンポーネントを作成（動作検証済）

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,27 +2,37 @@ import type { NextPage } from 'next'
 import Box from 'components/layout/Box'
 import Button from 'components/atoms/Button'
 import Text from 'components/atoms/Text'
+import Dropdown from 'components/molecules/Dropdown'
 
 
 const Test: NextPage = () => {
   return (
     <div>
-    <Box width="256px" height="80px" padding="16px">
+      <Box width="256px" height="80px" padding="16px">
         <Button>ボタン</Button>
-    </Box>
-    <Box width="256px" height="80px" padding="16px">
+      </Box>
+      <Box width="256px" height="80px" padding="16px">
         <Button variant="secondary">ボタン</Button>
-    </Box>
-    <Box width="256px" height="80px" padding="16px">
+      </Box>
+      <Box width="256px" height="80px" padding="16px">
         <Button variant="monotone">ボタン</Button>
-    </Box>
-    <Box width="256px" height="auto" padding="16px">
-      <Text>楽天会員1倍と楽天モバイルSPU＋4倍の合計値です。</Text>
-      <Text variant='extraSmall'>※楽天モバイルSPUの毎月の獲得上限ポイント数は2,000ポイント。期間限定ポイントでの付与。詳細はこちらをご確認ください。</Text>        
-    </Box>
-    <Box width="256px" height="auto" padding="16px">
+      </Box>
 
-    </Box>
+      <Box width="256px" height="auto" padding="16px">
+        <Text>楽天会員1倍と楽天モバイルSPU＋4倍の合計値です。</Text>
+        <Text variant='extraSmall'>※楽天モバイルSPUの毎月の獲得上限ポイント数は2,000ポイント。期間限定ポイントでの付与。詳細はこちらをご確認ください。</Text>
+      </Box>
+
+      <Box width="256px" height="auto" padding="16px">
+        <Dropdown
+          options={[
+            { id:1, value:'iPhone15ProMax', label:'iPhone15ProMax'},
+            { id:2, value:'iPhone15Pro', label:'iPhone15Pro'},
+          ]}
+          placeholder="製品を選択してください"
+          onChange={(v)=> console.log(v?.value)}
+        />
+      </Box>
     </div>
   )
 }
