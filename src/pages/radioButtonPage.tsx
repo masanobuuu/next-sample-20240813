dc23f50dcf54d7214997c95dc90a996827a1a7b8
MASANOBU\emasa <e.masanobuuu@gmail.com>
2024-08-15 08:50:50 +0900

    動作検証完了

--- src/pages/radioButtonPage.tsx
+++ src/pages/radioButtonPage.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import Box from 'components/layout/Box'
+import Text from 'components/atoms/Text'
 import React, { useState } from "react";
 import RadioButton from 'components/atoms/RadioButton'
 
 const RadioButtonPage: NextPage = () => {
     const [selectedOption, setSelectedOption] = useState("");
+    // const options = ["Option 1", "Option 2", "Option 3"];
     const options = ["Option 1", "Option 2", "Option 3"];
   
     const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,7 +19,9 @@ const RadioButtonPage: NextPage = () => {
           options={options}
           selectedOption={selectedOption}
           onChange={handleOptionChange}
+          fontSize='24px'
         />
+        <Text>選択中の項目：{selectedOption}</Text>
       </div>
     )
   }
